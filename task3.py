cb065a6f85bca2222594c965a22cde1807c473e8
valentinnsh <valentinnsh@gmail.com>
2019-10-13 16:21:05 +0300

    add task3

--- task3.py
+++ task3.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def get_indices(N, n_batches, split_ratio):
     """Generates splits of indices from 0 to N-1 into uniformly distributed\
        batches. Each batch is defined by 3 indices [i, j, k] where\
@@ -9,25 +10,31 @@ def get_indices(N, n_batches, split_ratio):
         N (int): total counts
         n_batches (int): number of splits
         split_ratio (float): split ratio, defines position of j in [i, j, k].
-
     Returns:
         generator for batch indices [i, j, k]
     """
-    inds = np.array([0, 0, 0])
+    j = (N-1) / (n_batches*split_ratio+1)
+    inds = np.array([0, j, (j+split_ratio*j)])
+
     for i in range(n_batches):
         # todo: move forward batch
         # calculate new indices
-        yield inds
+
+        yield np.array(inds, dtype=int)
+        inds += split_ratio*j
+        # inds = np.array([0, 0, 0])
+
 
 def main():
     for inds in get_indices(100, 5, 0.25):
         print(inds)
-    # expected result:
-    # [0, 44, 55]
-    # [11, 55, 66]
-    # [22, 66, 77]
-    # [33, 77, 88]
-    # [44, 88, 99]
+        # expected result:
+        # [0, 44, 55]
+        # [11, 55, 66]
+        # [22, 66, 77]
+        # [33, 77, 88]
+        # [44, 88, 99]
+
 
 if __name__ == "__main__":
     main()
