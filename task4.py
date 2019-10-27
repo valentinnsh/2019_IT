2cacf5fcc0791296fcc2272109b375af195b72b9
valentinnsh <valentinnsh@gmail.com>
2019-10-27 20:27:20 +0300

    Add solution for task4

---
+++ task4.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def gen_input_data(n, m, sp):
+    X = sp*np.random.random_sample((n, m)) - sp/2
+    return X
+
+
+def in_pareto_front(ind, X):
+    for i, element in enumerate(X):
+        if i != ind and not np.any(element < X[ind:]):
+            return False
+    return True
+
+
+if __name__ == "__main__":
+
+    m = 3   # number of axis
+    n = 7   # total vector num
+    sp = 100  # scale param
+    X = gen_input_data(n, m, sp)
+
+    front_ind = []
+    for i in range(X.shape[0]):
+        if in_pareto_front(i, X):
+            front_ind.append(i)
+
+    # X = np.append(X, np.reshape(X[:, 0], (n, 1)), axis=1)
+
+    pareto = X[front_ind, :]
+    ax_plt_angle = 2 * np.pi * np.arange(m + 1) / m
+
+    a, axes = plt.subplots(ncols=2, subplot_kw=dict(polar=True))
+
+    axes[0].set_title('Pareto front')
+    for vec in pareto:
+        axes[0].plot(ax_plt_angle, np.append(vec, vec[0]))
+
+    axes[1].set_title('All vectors')
+    for vec in X:
+        axes[1].plot(ax_plt_angle, np.append(vec, vec[0]))
+
+    plt.show()
